4c0821b26a6c996b7342da697275adcebdc0c10c
Wally Atkins <wallyatkins@gmail.com>
2025-07-09 17:00:52 -0400

    Add user menu and logout

---
+++ api/logout.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/auth.php';
+require_https();
+start_session();
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+    );
+}
+session_destroy();
+header('Content-Type: application/json');
+echo json_encode(['loggedOut' => true]);
+
